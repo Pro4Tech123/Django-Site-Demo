20a8e765a5da6716d6169d401ed62aeffd826da4
Pro4Tech123 <65108753+Pro4Tech123@users.noreply.github.com>
2020-05-13 23:27:53 +0500

    Add files via upload

---
+++ management/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Customer(models.Model):
+	user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+	name = models.CharField(max_length = 200, null = True)
+	email  = models.CharField(max_length = 200, null = True)
+	phone = models.CharField(max_length = 200, null = True)
+	date_created = models.DateTimeField(auto_now_add = True, null = True)
+	
+	def __str__(self):
+		return self.name
+
+
+class Category(models.Model):
+	name = models.CharField(max_length = 200, null = True)
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	name = models.CharField(max_length = 200, null = True)
+	price = models.FloatField(null = True)
+	description = models.CharField(max_length = 200, null = True, blank = True)
+	product_pic = models.ImageField(default = "images/breakfast-1.jpg", null = True, blank = True)
+	date_created = models.DateTimeField(auto_now_add = True, null = True)
+	categories = models.ManyToManyField(Category)
+
+	def __str__(self):
+		return self.name
+
+
+class Order(models.Model):
+
+	STATUS = (
+		('Pending', 'Pending'),
+		('Out For Delivery', 'Out For Delivery'),
+		('Delivered', 'Delivered')
+		)
+
+	customer = models.ForeignKey(Customer, null = True, on_delete = models.SET_NULL)
+	product = models.ForeignKey(Product, null = True, on_delete = models.SET_NULL)
+	status = models.CharField(max_length = 200, null = True, choices = STATUS, default = 'Pending')
+	date_created = models.DateTimeField(auto_now_add = True, null = True)
+	
+	def __str__(self):
+		return self.name
